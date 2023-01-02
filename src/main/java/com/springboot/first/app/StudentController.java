48604c91a9446e7827cbb4104e6f891872d05e33
Satyam Suman <findsatyam95@gmail.com>
2023-01-02 22:40:06 +0530

    Added basic rest api's to return list

--- src/main/java/com/springboot/first/app/StudentController.java
+++ src/main/java/com/springboot/first/app/StudentController.java
@@ -3,6 +3,10 @@ package com.springboot.first.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,5 +15,17 @@ public class StudentController {
         return new Student("Ramesh", "Kumar");
     }
 
+    @GetMapping("students")
+    public List<Student> getStrudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ramesh", "Kumar"));
+        students.add(new Student("Somesh", "Kumar"));
+        students.add(new Student("Lokesh", "Kumar"));
+        students.add(new Student("Sarvesh", "Kumar"));
+        students.add(new Student("Nikesh", "Kumar"));
+        students.add(new Student("Suresh", "Kumar"));
+
+        return students;
+    }
 
 }
