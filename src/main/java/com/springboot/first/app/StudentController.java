b966c98f7e08f7cbcb11a79b893235c01c745a8e
Satyam Suman <findsatyam95@gmail.com>
2023-01-02 22:40:55 +0530

    Removed unused imports

--- src/main/java/com/springboot/first/app/StudentController.java
+++ src/main/java/com/springboot/first/app/StudentController.java
@@ -3,7 +3,6 @@ package com.springboot.first.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.List;
 
